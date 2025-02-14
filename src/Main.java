f69e41dbd4cad09a706ea9722de399d21ae72491
Ekaterina Ushakova <liholat.ekaterina@gmail.com>
2025-02-14 19:57:38 +0500

    Working with numeric data types and mathematical operations on them

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-                String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число и нажмите <Enter>:");
+        int n1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите <Enter>:");
+        int n2 = new Scanner(System.in).nextInt();
+        int sum = n1 + n2;
+        int dif = n1 - n2;
+        int mult = n1 * n2;
+        double quot = ((double)n1 / n2);
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + dif);
+        System.out.println("Произведение: " + mult);
+        System.out.println("Частное: " + quot);
     }
 }
